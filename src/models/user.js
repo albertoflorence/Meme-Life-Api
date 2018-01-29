52a7f57674141a87802880350c884c2c44c775a4
albertoflorence <alberto.sabatier@gmail.com>
2018-01-29 16:17:46 -0200

    Implementation of user authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,45 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET, CRYPTO_SECRET } = require('../../config/SECRETS')
+const crypto = require('crypto')
+
 module.exports = Schema => {
   const userSchema = new Schema({
     name: String,
     avatar: String,
     email: String,
-    password: String
+    password: { type: String, select: false }
   })
 
+  userSchema.methods.getJwtToken = getJwtToken
+  userSchema.statics.login = function({ email, password }) {
+    const encryptPassword = passwordCrypt(password)
+    return this.findOne({ email, password: encryptPassword })
+  }
+
+  userSchema.pre('save', function(next) {
+    this.password = passwordCrypt(this.password)
+    next()
+  })
   return userSchema
 }
+
+function getJwtToken() {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      avatar: this.avatar,
+      name: this.name
+    },
+    JWT_SECRET,
+    {
+      expiresIn: '12h'
+    }
+  )
+}
+
+const passwordCrypt = password =>
+  crypto
+    .createHmac('sha256', CRYPTO_SECRET)
+    .update(password)
+    .digest('hex')
